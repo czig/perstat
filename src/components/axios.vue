82b6415071760550022e7107e2aeea36fd697a2f
Caleb <caleb.ziegler@gmail.com>
2017-12-14 13:01:30 -0600

    Adding axios.vue demo file for axios calls

---
+++ src/components/axios.vue
@@ -0,0 +1,43 @@
+<template>
+    <!--html here-->
+</template>
+<script>
+import { store } from '@/store'
+import { mapGetters } from 'vuex'
+
+export default{
+  data(){
+    return {
+      data: [],
+    }
+  },
+  computed: {
+    ndx(){
+      return crossfilter(this.data)
+    }
+  },
+  mounted: function(){
+    //axios call
+    window.axios.get('/endpoint').then(response => {
+        this.data = response.data.data
+        renderCharts()
+    }).catch(console.error)
+
+    //es6 arrow function
+      var renderCharts = () => {
+
+        // define charts here
+
+        //draw charts
+        dc.renderAll()
+        dc.redrawAll()
+      }
+  },
+  beforeDestroy: function(){
+    // This clears all registered chart. Otherwise, it'll keep appending charts on top of empty DOM
+    dc.chartRegistry.clear()
+  },
+}
+</script>
+<style>
+</style> 
