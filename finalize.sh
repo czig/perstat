6bdd162b0733d949d2e295af263b28234c3859af
rcena <renard.cenaj@gmail.com>
2018-04-05 10:38:33 -0500

    Added FLA build mode with FLA links

--- finalize.sh
+++ finalize.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+yarn run fla;
+mv ./fonts ./dist/static/.;
+rm ./dist/static/js/*.map;
+rm ./dist/static/*.png;
+rm ./dist/static/*.jpg;
+rm ./dist/static/*.JPG;
+
+rm -rf ../New_Push
+mkdir ../New_Push
+mkdir ../New_Push/FLA
+mkdir ../New_Push/PROD
+
+cp -a ./dist ../New_Push/FLA
+mv ../New_Push/FLA/dist ../New_Push/FLA/PERS_STAT
 
 yarn run build;
 mv ./fonts ./dist/static/.;
@@ -6,3 +20,6 @@ rm ./dist/static/js/*.map;
 rm ./dist/static/*.png;
 rm ./dist/static/*.jpg;
 rm ./dist/static/*.JPG;
+
+cp -a ./dist ../New_Push/PROD
+mv ../New_Push/PROD/dist ../New_Push/PROD/PERS_STAT
