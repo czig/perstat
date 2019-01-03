8850633e3460f81a43eca0025654343046562a75
Caleb <caleb.ziegler@gmail.com>
2019-01-03 09:32:54 -0600

    Updates fla.sh to do git pull in ./dist first

--- fla.sh
+++ fla.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
+#begin with git pull!
+git --git-dir=./dist/.git --work-tree=./dist pull origin master
+#build for fla (need fla endpoints)
 yarn run fla;
+#move/remove files for serving over apache
 mv ./fonts ./dist/static/.;
 rm ./dist/static/js/*.map;
 rm ./dist/static/*.png;
 rm ./dist/static/*.jpg;
 rm ./dist/static/*.JPG;
 cd ./dist;
-git pull origin master;
+#push code up to github ps_fla
 git add . ;
 git commit -m "`(date +%F%T)`";
 git push origin master;
