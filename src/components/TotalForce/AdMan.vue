885b01bdb3e3e9753b3069af4cf1b241483c2f58
mofongo74 <mad.gad.gomez@gmail.com>
2019-04-03 12:18:06 -0500

    More updates to repair font issue.

--- src/components/TotalForce/AdMan.vue
+++ src/components/TotalForce/AdMan.vue
@@ -472,12 +472,7 @@ import fab from 'vue-fab'
 <style src="../../../node_modules/dc/dc.css"/>
 <style>
 
-/*@font-face {
-  font-family: 'Material Icons';
-  font-style: normal;
-  font-weight: 400;
-  src: url('../../static/fonts/Material-Icons.d7e60f9.woff2') format('woff2');
-}*/
+/* @font-face{font-family:Material Icons;src:local(Material Icons),url(../../static/fonts/Material-Icons.d7e60f9.woff2) format("woff2");font-weight:400} }*/
 
 .material-icons {
   font-family: 'Material Icons';
